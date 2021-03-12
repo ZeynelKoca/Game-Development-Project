d436335239419f729fda503edcbab41589dd224f
Laurence Soutendijk <18028152@student.hhs.nl>
2021-03-12 21:40:14 +0100

    messing around

---
+++ Game Development Project/Assets/Scripts/BackForthMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackForthMovement : MonoBehaviour
+{
+    public float Speed = 2.5f;
+    public float Distance = 5;
+    float startZ;
+    float addZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addZ = Mathf.PingPong(Time.time * Speed, Distance);
+        transform.position = new Vector3(transform.position.x,transform.position.y,startZ + addZ);
+        
+    }
+}
---
+++ Game Development Project/Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    public float PushPower = 2;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        //Object does not have a rigidbody
+        if(body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        //Objects don't get pushed below the player
+        if(hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+        //Checks what direction the players hits the object
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        //Applies the pushpower to the direction
+        body.velocity = pushDirection * PushPower;
+    }
+
+}
