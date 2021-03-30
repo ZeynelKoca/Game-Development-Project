81b86243ea2739168ceda0a41ba52ed00872fc5d
Laurence Soutendijk <18028152@student.hhs.nl>
2021-03-30 11:05:19 +0200

    Added trigger for npc interaction
    You can now hit E while near the npc and it will change its color

---
+++ Game Development Project/Assets/Scripts/Trigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Npc;
+    private bool _triggerActive = false;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _triggerActive = true;
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _triggerActive = false;
+        }
+    }
+    public void Update()
+    {
+        if(_triggerActive && Input.GetKeyDown(KeyCode.E))
+        {
+            int numberOfChildren = Npc.transform.childCount;
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                GameObject child = Npc.transform.GetChild(i).gameObject;
+                child.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+        }
+        if (!_triggerActive && Input.GetKeyDown(KeyCode.E))
+        {
+            int numberOfChildren = Npc.transform.childCount;
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                GameObject child = Npc.transform.GetChild(i).gameObject;
+                child.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
+
