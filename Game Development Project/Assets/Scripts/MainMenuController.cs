0938e28f4084042fddb03ccd3d28d5992e42c744
Zeynel <zeynel.koca@outlook.com>
2021-04-01 16:26:50 +0200

    Added MainMenu scene and working buttons

---
+++ Game Development Project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Game Development Project/Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SettingsButton : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+
+    public void ShowSettingsPanel()
+    {
+        SettingsPanel.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        SettingsPanel.SetActive(false);
+    }
+}
