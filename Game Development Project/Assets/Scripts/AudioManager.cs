d966309196ce379fa5656fed664244bcf5e64469
Zeynel <zeynel.koca@outlook.com>
2021-04-02 00:17:50 +0200

    Settings prefab and resolutions dropdown start

---
+++ Game Development Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource _backgroundAudio;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backgroundAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PauseMenu.GamePausedState)
+        {
+            _backgroundAudio.volume = Settings.VolumeSetting / 3;
+        }
+        else
+        {
+            _backgroundAudio.volume = Settings.VolumeSetting;
+        }
+    }
+}
---
+++ Game Development Project/Assets/Scripts/Settings.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Settings : MonoBehaviour
+    {
+        public GameObject SettingsPanel;
+        public Slider VolumeSlider;
+        public Slider BrightnessSlider;
+        public Toggle FullscreenToggle;
+
+        public static float VolumeSetting => PlayerPrefs.GetFloat("SoundVolume", 0.8f);
+
+        public static float BrightnessSetting => PlayerPrefs.GetFloat("Brightness", 0.9f);
+
+        public static bool FullscreenSetting => PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            VolumeSlider.value = VolumeSetting;
+            BrightnessSlider.value = BrightnessSetting;
+            FullscreenToggle.isOn = FullscreenSetting;
+            Screen.fullScreen = FullscreenSetting;
+        }
+
+        public void ShowSettingsPanel()
+        {
+            SettingsPanel.SetActive(true);
+        }
+
+        public void CloseSettingsPanel()
+        {
+            SettingsPanel.SetActive(false);
+        }
+
+        public void SetVolumeSetting()
+        {
+            PlayerPrefs.SetFloat("SoundVolume", VolumeSlider.value);
+        }
+
+        public void SetBrightnessSetting()
+        {
+            PlayerPrefs.SetFloat("Brightness", BrightnessSlider.value);
+        }
+
+        public void SetFullscreenSetting()
+        {
+            PlayerPrefs.SetInt("Fullscreen", FullscreenToggle.isOn ? 1 : 0);
+        }
+    }
+}
