e2e3ab25d865fd516318f02dbeb2044bcb6015b0
Zeynel <zeynel.koca@outlook.com>
2021-04-02 15:04:59 +0200

    Resolution dropdown fixed

--- Game Development Project/Assets/Scripts/Settings.cs
+++ Game Development Project/Assets/Scripts/Settings.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
@@ -9,6 +11,9 @@ namespace Assets.Scripts
         public Slider VolumeSlider;
         public Slider BrightnessSlider;
         public Toggle FullscreenToggle;
+        public Dropdown ResolutionsDropdown;
+
+        private Resolution[] _resolutions;
 
         public static float VolumeSetting => PlayerPrefs.GetFloat("SoundVolume", 0.8f);
 
@@ -16,13 +21,50 @@ namespace Assets.Scripts
 
         public static bool FullscreenSetting => PlayerPrefs.GetInt("Fullscreen", 1) == 1;
 
+        public static int ResolutionSetting => PlayerPrefs.GetInt("Resolution", -1);
+
         // Start is called before the first frame update
         void Start()
         {
             VolumeSlider.value = VolumeSetting;
             BrightnessSlider.value = BrightnessSetting;
             FullscreenToggle.isOn = FullscreenSetting;
+            CalculateResolutions();
+
+            InitializeGameSettings();
+        }
+
+        private void InitializeGameSettings()
+        {
             Screen.fullScreen = FullscreenSetting;
+
+            var currentResolution = _resolutions[ResolutionSetting];
+            Screen.SetResolution(currentResolution.width, currentResolution.height, FullscreenSetting);
+        }
+
+        private void CalculateResolutions()
+        {
+            _resolutions = Screen.resolutions;
+            ResolutionsDropdown.ClearOptions();
+
+            Resolution currentResolution = new Resolution();
+            List<string> dropdownOptions = new List<string>();
+            foreach (var resolution in _resolutions)
+            {
+                dropdownOptions.Add($"{resolution.width} x {resolution.height}");
+
+                if (resolution.height == Screen.currentResolution.height &&
+                    resolution.width == Screen.currentResolution.width)
+                {
+                    currentResolution = resolution;
+                }
+            }
+
+            ResolutionsDropdown.AddOptions(dropdownOptions);
+            ResolutionsDropdown.value = ResolutionSetting == -1
+                ? Array.IndexOf(_resolutions, currentResolution)
+                : ResolutionSetting;
+            ResolutionsDropdown.RefreshShownValue();
         }
 
         public void ShowSettingsPanel()
@@ -48,6 +90,15 @@ namespace Assets.Scripts
         public void SetFullscreenSetting()
         {
             PlayerPrefs.SetInt("Fullscreen", FullscreenToggle.isOn ? 1 : 0);
+            Screen.fullScreen = FullscreenSetting;
+        }
+
+        public void SetResolutionSetting()
+        {
+            PlayerPrefs.SetInt("Resolution", ResolutionsDropdown.value);
+
+            var currentResolution = _resolutions[ResolutionSetting];
+            Screen.SetResolution(currentResolution.width, currentResolution.height, FullscreenSetting);
         }
     }
 }
