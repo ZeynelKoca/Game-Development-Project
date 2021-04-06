086f6e7a0fc992f3257f312526922a6970128654
Laurence Soutendijk <18028152@student.hhs.nl>
2021-04-06 15:29:46 +0200

    Finished the base for npc interaction
    When you press E near the npc the game pauses and you can enter the dialog with the npc, once the dialog is done the game unpauses and you can interact again with the npc

---
+++ Game Development Project/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class InteractableObject :MonoBehaviour
+    {
+        public Camera Camera;
+        public Vector3 PlayerPostion;
+        public GameObject Object;
+        public bool DialogDone { get; private set; } = false;
+        private int _currentDialog = 0;
+
+        public void Talk(Text text)
+        {
+            DialogDone = false;
+            switch (_currentDialog)
+            {
+                case 0:
+                    {
+                        text.text = "Yeet";
+                        _currentDialog++;
+                        break;
+                    }
+                case 1:
+                    {
+                        text.text = "Double Yeet";
+                        _currentDialog++;
+                        break;
+                    }
+                case 2:
+                    {
+                        text.text = @"
+                                    You know they say that all men are created equal,
+                                    but you look at me and you look at Samoa Joe and you can see that statement
+                                    is not true.
+                                    See, normally if you go one on one with another wrestler,
+                                    you got a 50/50 chance of winning.
+                                    But I'm a genetic freak and I'm not normal!
+                                    So you got a 25%, AT BEST, at beat me.
+                                    Then you add Kurt Angle to the mix, your chances of winning drastic go down.
+                                    See the 3 way at Sacrifice, you got a 33 1/3 chance of winning,
+                                    but I, I got a 66 and 2/3 chance of winning,
+                                    because Kurt Angle KNOWS he can't beat me and he's not even gonna try!
+                                    So Samoa Joe, you take your 33 1/3 chance,
+                                    minus my 25% chance and you got an 8 1/3 chance of winning at Sacrifice.
+                                    But then you take my 75% chance of winning, if we was to go one on one,
+                                    and then add 66 2/3 per cents,
+                                    I got 141 2/3 chance of winning at Sacrifice.
+                                    See Joe, the numbers don't lie, and they spell disaster for you at Sacrifice.
+                                    ";
+                        _currentDialog++;
+                        break;
+                    }
+                case 3:
+                    {
+                        text.text = "ByeBye";
+                        _currentDialog = 0;
+                        DialogDone = true;
+                        break;
+                    }
+            }
+        }
+    }
+
+}
--- Game Development Project/Assets/Scripts/Trigger.cs
+++ Game Development Project/Assets/Scripts/Trigger.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour
 {
-    public GameObject Npc;
+    public InteractableObject Npc;
+    public Text text;
+    private GameObject Player;
+    public Camera MainCamera;
+    private bool _gamePaused = false;
     private bool _triggerActive = false;
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -23,23 +33,47 @@ public class Trigger : MonoBehaviour
     }
     public void Update()
     {
-        if(_triggerActive && Input.GetKeyDown(KeyCode.E))
+        if (_triggerActive && !_gamePaused)
         {
-            int numberOfChildren = Npc.transform.childCount;
+            text.text = "Press E to interact";
+            text.enabled = true;
+        }
+        if(_triggerActive && Input.GetKeyDown(KeyCode.E) && !_gamePaused)
+        {
+            int numberOfChildren = Npc.Object.transform.childCount;
+            text.enabled = true;
+            Npc.Talk(text);
             for (int i = 0; i < numberOfChildren; i++)
             {
-                GameObject child = Npc.transform.GetChild(i).gameObject;
+                GameObject child = Npc.Object.transform.GetChild(i).gameObject;
                 child.GetComponent<Renderer>().material.color = Color.yellow;
+                MainCamera.enabled = false;
+                Npc.Camera.enabled = true;
+                _gamePaused = true;
+                Player.transform.position = Npc.PlayerPostion;
+                Time.timeScale = 0f;
             }
         }
-        if (!_triggerActive && Input.GetKeyDown(KeyCode.E))
+        if (_gamePaused && Input.GetKeyDown(KeyCode.E))
         {
-            int numberOfChildren = Npc.transform.childCount;
-            for (int i = 0; i < numberOfChildren; i++)
+            if(Npc.DialogDone)
             {
-                GameObject child = Npc.transform.GetChild(i).gameObject;
-                child.GetComponent<Renderer>().material.color = Color.red;
+
+                MainCamera.enabled = true;
+                Npc.Camera.enabled = false;
+                text.enabled = false;
+                _gamePaused = false;
+                Time.timeScale = 1f;
             }
+            else
+            {
+                Npc.Talk(text);
+            }
+
+        }
+        if (!_triggerActive)
+        {
+            text.enabled = false;
         }
     }
 }
